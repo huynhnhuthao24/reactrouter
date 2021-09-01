6791c20c353af3417d25dea66723a83b2f5963eb
Asland <huynhnhuth53@gmail.com>
2021-09-01 16:55:06 +0700

    push code react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Home from "./Components/Home";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+        <div className="App">
+           <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+               <div className="container-fluid">
+                   <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                       data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                       aria-label="Toggle navigation">
+                       <span className="navbar-toggler-icon"></span>
+                   </button>
+                   <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                           <li className="nav-item">
+                               <NavLink activeClassName="active" exact to="/" className="nav-link ">Home</NavLink>
+                           </li>
+                           <li className="nav-item">
+                                <NavLink activeClassName="active" to="/About" className="nav-link">About</NavLink>
+                           </li>
+                           <li className="nav-item">
+                                <NavLink activeClassName="active" to="/Contact" className="nav-link">Contact</NavLink>
+                           </li>
+                       </ul>
+                   </div>
+               </div>
+           </nav>
+           <Route exact path="/" component={Home} />
+           <Route path="/About" component={About} />
+           <Route path="/Contact" component={Contact} />
     </div>
+    </Router>
   );
 }
 
